b51874d6c6b0d76360b48ccc43183059a74dcfd1
shubhamgoswami-official <shubham.goswami@mobcoder.com>
2019-03-03 16:22:14 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+videoUpload(event){
+
+var reader = new FileReader();
+reader.onload = function () {
+    var output = document.getElementById('video');
+    output.src = reader.result;
+};
+reader.readAsDataURL(event.target.files[0]);
+}
+
+
+
   render() {
     return (
       <div className="App">
+      <center>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+
+<input id="abc" type="file"  onChange={(event)=>this.videoUpload(event)}/>
+<video id="video" width="520" height="440" ></video>
+
+
         </header>
+        </center>
+
       </div>
     );
   }
